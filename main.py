611c8435283d6c5da1481958f0b0247d60423704
ikz87 <iikz87ii@gmail.com>
2023-06-09 00:29:18 -0600

    Start using CircuitPython

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import usb_hid
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
 def main():
+    # SETUP
+    kbd = Keyboard(usb_hid.devices)
+    switch = DigitalInOut(board.GP0)
+    switch.direction = Direction.INPUT
+    switch.pull = Pull.DOWN
+
+    # LOOP
+    # Testing with a digital switch
     while True:
-        print("now we define a main function")
-        sleep(1)
+        if switch.value:
+            kbd.press(Keycode.A)
+        else:
+            kbd.release(Keycode.A)
+        # Release all keys.
+        #kbd.release_all()
+        pass
+
 
 if __name__ == "__main__":
     main()

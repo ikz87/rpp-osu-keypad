32cf98e8d19765a5af610ea3d7e12e16ae86a6d2
ikz87 <iikz87ii@gmail.com>
2023-06-11 01:50:13 -0600

    Temporary implementation of a one time calibration

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import usb_hid
 import board
-from digitalio import DigitalInOut, Direction, Pull
-import analogio
+import time
 import keys
+from digitalio import DigitalInOut, Direction, Pull
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
 def main():
     # SETUP
     kbd = Keyboard(usb_hid.devices)
-    keylist = [keys.key(id=1, pin=board.GP28, keycode=Keycode.S),
-               keys.key(id=2, pin=board.GP27, keycode=Keycode.D)]
-    # LOOP
-    while True:
+    keylist = [keys.Key(id=1, pin=board.GP28, keycode=Keycode.S),
+               keys.Key(id=2, pin=board.GP27, keycode=Keycode.D)]
+
+    # TEMPORARY SOLUTION FOR ONE TIME CALIBRATION
+    for i in range(1000):
         for key in keylist:
             key.poll()
             key.calibrate()
-            key.rapid_trigger()
-            if key.curr_state:
-                if key.state_changed:
-                    kbd.press(key.keycode)
-                    #print("Press")
-                    pass
-            else:
-                if key.state_changed:
-                    kbd.release(key.keycode)
-                    #print("Release")
-                    pass
-            #key.fixed_actuation()
+            time.sleep(0.01)
+            print("calibrating", i)
 
+
+    # LOOP
+    while True:
+        for key in keylist:
+                key.poll()
+                key.rapid_trigger()
+                if key.curr_state:
+                    if key.state_changed:
+                        kbd.press(key.keycode)
+                        #print("Press")
+                        pass
+                else:
+                    if key.state_changed:
+                        kbd.release(key.keycode)
+                        pass
         pass
 if __name__ == "__main__":
     main()

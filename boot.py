ea197c06970c03f6676d97f46b22193c025a2d0c
ikz87 <iikz87ii@gmail.com>
2023-06-20 23:09:26 -0600

    Rename device

--- boot.py
+++ boot.py
@@ -6,6 +6,12 @@ from digitalio import DigitalInOut, Direction, Pull
 # Disable autoreloading jic
 supervisor.runtime.autoreload = False
 
+# Change volume name
+new_name = "KZOOTING"
+storage.remount("/", readonly=False)
+m = storage.getmount("/")
+m.label = new_name
+
 # Calibration mode
 calibration_button = DigitalInOut(board.GP15)
 calibration_button.direction = Direction.INPUT
@@ -14,3 +20,5 @@ calibration_button.pull = Pull.DOWN
 if calibration_button.value:
     storage.remount("/", readonly=False)
     supervisor.set_next_code_file("calibration.py")
+else:
+    storage.remount("/", readonly=True)
